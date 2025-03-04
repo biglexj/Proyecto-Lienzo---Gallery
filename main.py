d18a8679ecf99a8c716baf5dd6d2474b417987df
Biglex Dev <biglex.rv@gmail.com>
2025-03-03 21:42:17 -0500

    first commit - flet

---
+++ main.py
@@ -0,0 +1,10 @@
+import flet as ft
+from home.home import HomeScreen
+
+def main(page: ft.Page):
+    page.title = "Galería"
+    page.window_width = 500
+    page.window_height = 800
+    HomeScreen(page)
+
+ft.app(target=main)
